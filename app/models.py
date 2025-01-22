057040a3b398575590e1f106d350e5f9cf67c319
danakimm <danakim917@gmail.com>
2025-01-22 20:55:34 +0900

    minor

--- app/models.py
+++ app/models.py
@@ -85,7 +85,7 @@ class TrafficPrediction(Base):
     prediction_5min = Column(Numeric(5,1), nullable=False)
     prediction_10min = Column(Numeric(5,1), nullable=False)
     prediction_15min = Column(Numeric(5,1), nullable=False)
-
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
 
 
 class linkidsortorder(Base):
